3630c8c565d99ffa89f9accff0e95d823b4d1f77
ShoukatSifat <shoukatalamsifat@gmail.com>
2023-08-22 14:31:07 +0600

    fgh

--- Screen/HomeScreen.js
+++ Screen/HomeScreen.js
@@ -5,11 +5,26 @@ import { ScrollView,View, StyleSheet, Text, ImageBackground, TouchableOpacity } 
 function HomeScreen() {
     const navigation = useNavigation();
   return (
-    <View style={styles.container}>
-        {/* <View style={styles.header}>
-          <Text style={styles.title}>Islamic Hub</Text>
-        </View> */}
+    <ScrollView style={styles.container}>
+        <View style={styles.header}>
+          <Text style={styles.title}>MINNAH</Text>
+        </View>
         <View style={styles.buttonContainer}>
+
+          <TouchableOpacity
+              style={styles.button}
+              onPress={() => navigation.navigate('QuranChapters')}
+            >
+              <ImageBackground
+                source={{ uri: 'https://i0.wp.com/islamtheultimatepeace.com/wp-content/uploads/2021/09/holy-quran.jpeg?fit=2000%2C1333&ssl=1' }}
+                style={styles.buttonBackground}
+                imageStyle={{ resizeMode: 'cover' }}
+              >
+                <Text style={styles.buttonText}>Al-Quran</Text>
+              </ImageBackground>
+            </TouchableOpacity>
+
+
             <TouchableOpacity style={styles.button}
                 onPress={() => navigation.navigate('ZakatCalculator')}
                 >
@@ -21,20 +36,7 @@ function HomeScreen() {
                 </ImageBackground>
             </TouchableOpacity>
 
-            <TouchableOpacity
-            style={styles.button}
-            onPress={() => navigation.navigate('QuranChapters')}
-          >
-            <ImageBackground
-              source={{ uri: 'https://i0.wp.com/islamtheultimatepeace.com/wp-content/uploads/2021/09/holy-quran.jpeg?fit=2000%2C1333&ssl=1' }}
-              style={styles.buttonBackground}
-              imageStyle={{ resizeMode: 'cover' }}
-            >
-              <Text style={styles.buttonText}>Al-Quran</Text>
-            </ImageBackground>
-          </TouchableOpacity>
-
-
+          
           <TouchableOpacity
             style={styles.button}
             onPress={() => navigation.navigate('TasbihScreen')}
@@ -51,27 +53,27 @@ function HomeScreen() {
 
           <TouchableOpacity
             style={styles.button}
-            // onPress={() => console.log('Alarm Pressed')}
+            onPress={() => console.log('Alarm Pressed')}
           >
             <ImageBackground
-              source={{ uri: 'https://media.istockphoto.com/id/1173500687/vector/red-alarm-clock-isolated-on-blue-background.jpg?s=612x612&w=0&k=20&c=Ttc6l3lBmrtOmwfTPruG6jmMGj5Y2dPV8OPsQjjYw0E=' }}
+              source={{ uri: 'https://play-lh.googleusercontent.com/4TqBPBBjEtznZ5O0mTvBsWqeLiTixzT0KRz4X8LG0rYUZWUe8MwJ3ztOlrKoXzE8yiE' }}
               style={styles.buttonBackground}
               imageStyle={{ resizeMode: 'cover' }}
             >
-              <Text style={styles.buttonText}>Alarm</Text>
+              <Text style={styles.buttonText}>Alarm for Salah</Text>
             </ImageBackground>
           </TouchableOpacity>
 
        
           <TouchableOpacity
-            style={[{ backgroundColor: '#B5C7E6', height:30 }]}
+            style={[{ backgroundColor: '#B5C7E6', marginBottom:10, }]}
             onPress={() => navigation.navigate('Donation')}
           >
             <Text style={styles.buttonText}>I Want to Donate</Text>
           </TouchableOpacity>
 
         </View>
-    </View>
+    </ScrollView>
   );
 }
 
@@ -79,25 +81,26 @@ const styles = StyleSheet.create({
     container: {
       flex: 1,
       backgroundColor: '#282828',
+
     },
     background: {
-      flex: 5,
+      flex: 1,
     },
     header: {
       alignItems: 'center',
       justifyContent: 'center',
-      marginTop: 50,
+      marginTop: 10,
     },
     title: {
       color: '#FFFFFF',
-      fontSize: 36,
+      fontSize: 38,
       fontWeight: 'bold',
       textShadowColor: '#000000',
       textShadowOffset: {
         width: 2,
         height: 2,
       },
-      textShadowRadius: 5,
+      textShadowRadius: 10,
     },
     buttonContainer: {
       flex: 1,
@@ -110,13 +113,13 @@ const styles = StyleSheet.create({
       height: 150,
       justifyContent: 'flex-end',
       borderRadius: 15,
-      marginBottom: 5,
-    //   overflow: 'hidden',
+      marginBottom: 15,
+      overflow: 'scroll',
     },
     buttonBackground: {
       flex: 1,
       justifyContent: 'flex-end',
-      padding: 10,
+      padding: 15,
     },
     buttonText: {
       color: '#FFFFFF',
@@ -128,7 +131,7 @@ const styles = StyleSheet.create({
         width: 2,
         height: 2,
       },
-      textShadowRadius: 5,
+      
     },
   });
 
