29ae5be19a7b58b2899863628d47e11e08a79d42
Fiasco <Fiasco@gmail.com>
2023-08-29 03:37:44 +0600

    tasbih delete

--- Screen/Tasbih.js
+++ Screen/Tasbih.js
@@ -25,18 +25,24 @@ const TasbihScreen = () => {
     }
   }
 
+  const handleDeleteNote = (id) => {
+    const updatedNotes = notes.filter(item => item.id !== id);
+    setNotes(updatedNotes);
+  }
+
   return (
     <View style={styles.container}>
       <View style={styles.tasbihContainer}>
         <Text style={styles.count}>{count}</Text>
         
-        <View style={styles.buttonsContainer}>
-          <TouchableOpacity style={[styles.button, styles.resetButton]} onPress={handleReset}>
+        <View style={{flexDirection: 'row'}}>
+        <TouchableOpacity style={[styles.button, styles.resetButton]} onPress={handleReset}>
             <Text style={[styles.buttonText, styles.resetButtonText]}>Reset</Text>
-          </TouchableOpacity>
-          <TouchableOpacity style={styles.button} onPress={handleCount}>
+        </TouchableOpacity>
+        <TouchableOpacity style={styles.button} onPress={handleCount}>
             <Text style={styles.buttonText}>Count</Text>
-          </TouchableOpacity>
+        </TouchableOpacity>
+        
         </View>
             
       </View>
@@ -58,6 +64,12 @@ const TasbihScreen = () => {
           renderItem={({ item }) => (
             <View style={styles.noteItem}>
               <Text style={styles.noteText}>{item.text}</Text>
+              <TouchableOpacity
+                style={styles.deleteButton}
+                onPress={() => handleDeleteNote(item.id)}
+              >
+                <Text style={styles.deleteButtonText}>Delete</Text>
+              </TouchableOpacity>
             </View>
           )}
         />
@@ -67,94 +79,105 @@ const TasbihScreen = () => {
 }
 
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#1F1F1F',
-    padding: 20,
-  },
-  tasbihContainer: {
-    flex: 1,
-    justifyContent: 'center',
-    alignItems: 'center',
-  },
-  buttonsContainer: {
-    flexDirection: 'row',
-  },
-  notesContainer: {
-    flex: 1,
-    alignSelf: 'stretch',
-    marginVertical: 32,
-  },
-  count: {
-    fontSize: 72,
-    marginBottom: 32,
-    color: '#FFFFFF',
-    textAlign: 'center',
-    textShadowColor: '#000000',
-    textShadowOffset: {
-      width: 2,
-      height: 2,
+    container: {
+      flex: 1,
+      backgroundColor: '#1F1F1F',
+      padding: 20,
     },
-    textShadowRadius: 5,
-  },
-  button: {
-    backgroundColor: '#4CAF50',
-    borderRadius: 9999,
-    width: 120,
-    height: 120,
-    alignItems: 'center',
-    justifyContent: 'center',
-    marginBottom: 16,
-    marginHorizontal: 25,
-  },
-  buttonText: {
-    color: '#FFFFFF',
-    fontSize: 24,
-  },
-  resetButton: {
-    backgroundColor: '#FF5722',
-    borderRadius: 8,
-    paddingHorizontal: 16,
-    paddingVertical: 12,
-    marginBottom: 16,
-    alignItems: 'center',
-  },
-  resetButtonText: {
-    color: '#FFFFFF',
-    fontSize: 18,
-  },
-  noteInput: {
-    height: 48,
-    borderColor: '#CCCCCC',
-    borderWidth: 1,
-    borderRadius: 8,
-    paddingHorizontal: 16,
-    marginBottom: 16,
-    color: '#FFFFFF',
-  },
-  addNoteButton: {
-    backgroundColor: '#4CAF50',
-    borderRadius: 8,
-    paddingHorizontal: 16,
-    paddingVertical: 12,
-    marginBottom: 16,
-    alignItems: 'center',
-  },
-  notesList: {
-    flex: 1,
-    marginVertical: 16,
-  },
-  noteItem: {
-    backgroundColor: '#2C2C2C',
-    borderRadius: 8,
-    paddingHorizontal: 16,
-    paddingVertical: 12,
-    marginBottom: 8,
-  },
-  noteText: {
-    fontSize: 18,
-    color: '#fff',
-  },
+    tasbihContainer: {
+      flex: 1,
+      justifyContent: 'center',
+      alignItems: 'center',
+    },
+    notesContainer: {
+      flex: 1,
+      alignSelf: 'stretch',
+      marginVertical: 32,
+    },
+    count: {
+      fontSize: 72,
+      marginBottom: 32,
+      color: '#FFFFFF',
+      textAlign: 'center',
+      textShadowColor: '#000000',
+      textShadowOffset: {
+        width: 2,
+        height: 2,
+      },
+      textShadowRadius: 5,
+    },
+    button: {
+        backgroundColor: '#4CAF50',
+        borderRadius: 9999,
+        width: 120,
+        height: 120,
+        alignItems: 'center',
+        justifyContent: 'center',
+        marginBottom: 16,
+        marginHorizontal: 25,
+      },
+    buttonText: {
+      color: '#FFFFFF',
+      fontSize: 24,
+    },
+    resetButton: {
+      backgroundColor: '#FF5722',
+      borderRadius: 8,
+      paddingHorizontal: 16,
+      paddingVertical: 12,
+      marginBottom: 16,
+      alignItems: 'center',
+    },
+    resetButtonText: {
+      color: '#FFFFFF',
+      fontSize: 18,
+    },
+    noteInput: {
+      height: 48,
+      borderColor: '#CCCCCC',
+      borderWidth: 1,
+      borderRadius: 8,
+      paddingHorizontal: 16,
+      marginBottom: 16,
+      color: '#FFFFFF',
+      
+    },
+    addNoteButton: {
+      backgroundColor: '#4CAF50',
+      borderRadius: 8,
+      paddingHorizontal: 16,
+      paddingVertical: 12,
+      marginBottom: 16,
+      alignItems: 'center',
+    },
+    notesList: {
+      flex: 1,
+      marginVertical: 16,
+    },
+    noteItem: {
+      flexDirection: 'row',
+      alignItems: 'center',
+      justifyContent: 'space-between',
+      backgroundColor: '#2C2C2C',
+      borderRadius: 8,
+      paddingHorizontal: 16,
+      paddingVertical: 12,
+      marginBottom: 8,
+    },
+    noteText: {
+      fontSize: 18,
+      color: '#fff',
+    },
+    deleteButton: {
+      backgroundColor: '#FF5722',
+      borderRadius: 8,
+      paddingHorizontal: 12,
+      paddingVertical: 6,
+    },
+    deleteButtonText: {
+      color: '#FFFFFF',
+      fontSize: 14,
+    },
 });
 
 export default TasbihScreen;
